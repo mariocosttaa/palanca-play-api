c70b5114a4f98ad0d34dfa88a194f7cf43a11e4f
Mário Costa <mariocostaa6@gmail.com>
2025-11-19 03:04:32 +0000

    feat: add specific API resources for authentication responses
    - Create UserResourceSpecific for detailed user data
    - Create BusinessUserResourceSpecific for detailed business user data

---
+++ app/Http/Resources/Specific/BusinessUserResourceSpecific.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources\Specific;
+
+use App\Actions\General\EasyHashAction;
+use App\Http\Resources\General\CountryResourceGeneral;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BusinessUserResourceSpecific extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => EasyHashAction::encode($this->id, 'business-user-id'),
+            'name' => $this->name,
+            'surname' => $this->surname,
+            'email' => $this->email,
+            'google_login' => $this->google_login,
+            'country_id' => $this->country_id
+                ? EasyHashAction::encode($this->country_id, 'country-id')
+                : null,
+            'country' => new CountryResourceGeneral($this->whenLoaded('country')),
+            'calling_code' => $this->calling_code,
+            'phone' => $this->phone,
+            'timezone' => $this->timezone,
+            'created_at' => $this->created_at?->toISOString(),
+            'updated_at' => $this->updated_at?->toISOString(),
+        ];
+    }
+}
+
---
+++ app/Http/Resources/Specific/UserResourceSpecific.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources\Specific;
+
+use App\Actions\General\EasyHashAction;
+use App\Http\Resources\General\CountryResourceGeneral;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResourceSpecific extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => EasyHashAction::encode($this->id, 'user-id'),
+            'name' => $this->name,
+            'surname' => $this->surname,
+            'email' => $this->email,
+            'google_login' => $this->google_login,
+            'country_id' => $this->country_id
+                ? EasyHashAction::encode($this->country_id, 'country-id')
+                : null,
+            'country' => new CountryResourceGeneral($this->whenLoaded('country')),
+            'calling_code' => $this->calling_code,
+            'phone' => $this->phone,
+            'timezone' => $this->timezone,
+            'email_verified_at' => $this->email_verified_at?->toISOString(),
+            'created_at' => $this->created_at?->toISOString(),
+            'updated_at' => $this->updated_at?->toISOString(),
+        ];
+    }
+}
+
