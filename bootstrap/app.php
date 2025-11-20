84da956ba7db13a0ed09a1f14cd2e41aeb54b071
Mário Costa <mariocostaa6@gmail.com>
2025-11-20 02:52:42 +0000

    chore: update dependencies and bootstrap configuration
    - Update composer.json with new dependencies
    - Update composer.lock
    - Update bootstrap/app.php configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'tenant.show' => \App\Http\Middleware\EnsureTenantAccess::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
